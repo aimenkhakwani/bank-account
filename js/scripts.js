f8ca84c4918f29d6f15c107ad4f7a4b1687459cd
Aimen Khakwani and Joanna Anderson <joanna@>
2016-08-23 15:22:44 -0700

    Adds functionality to show user accoutn balance

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,52 @@
 // business logic
-var inputName = "";
-var inputInitialDeposit = 0;
-var bankCustomer = {};
-var inputWithdrawl = 0;
-var inputDeposit = 0;
-
 function Bankaccount(name, deposit){
   this.userName = name;
-  this.userInitialDeposit = deposit;
-  this.userBalance = 0;
-  this.userDeposit = 0;
-  this.userWithdrawl = 0;
+  this.userBalance = deposit;
+}
+Bankaccount.prototype.showBalance = function(){
+  return "Hi " + this.userName + " your current balance is " + this.userBalance;
 }
 
-Bankaccount.prototype.calculateBalance = function(){
-  var balance = 0;
-  if((this.userDeposit > 0) && (this.userWithdrawl > 0)) {
-    balance = this.userInitialDeposit - this.userWithdrawl + this.userDeposit;
-  }
-  else if((this.userDeposit === NaN) && (this.userWithdrawl > 0)){
-    balance =
-  }
-  else if deposit has a value
-  balance = initial deposit + deposit;
-  else if withdrawal has a value{
-   balance =   initial deposit - withdrawal
-  }
-  this.userBalance = balance ;
+Bankaccount.prototype.addBalance = function(deposit){
+  this.userBalance = this.userBalance + deposit;
+  return this.userBalance;
+}
+
+Bankaccount.prototype.subtractBalance = function(withdrawl){
+  this.userBalance = this.userBalance - withdrawl;
+  return this.userBalance;
 }
 
 // ui logic
 $(".createAccount").submit(function(event){
   event.preventDefault();
-  inputName = $("#name").val();
-  inputInitialDeposit = parseInt($("#initialDeposit").val());
+  // $(".userBalance").empty();
+  var inputName = $("#name").val();
+  var inputInitialDeposit = parseFloat($("#initialDeposit").val());
+  var bankCustomer = new Bankaccount(inputName, inputInitialDeposit);
+  $(".userBalance").text(bankCustomer.showBalance());
 
-  bankCustomer = new Bankaccount(inputName, inputInitialDeposit);
-  console.log(bankCustomer);
-});
+  $(".depositOrWithdraw").submit(function(event){
+    debugger;
+    event.preventDefault();
 
-$(".depositOrWithdraw").submit(function(event){
-  event.preventDefault();
-  inputWithdrawl = parseInt($("#withdrawl").val());
-  inputDeposit = parseInt($("#deposit").val());
+    var inputWithdrawl = parseFloat($("input#withdrawl").val());
+    var inputDeposit = parseFloat($("input#deposit").val());
+
+    if ((inputWithdrawl === NaN) && (inputDeposit === NaN)){
+      alert("Please enter a value.");
+    }
+    else if ((inputWithdrawl === NaN) && (inputDeposit > 0)){
+      bankCustomer.addBalance(inputDeposit);
+    }
+    else if ((inputDeposit === NaN) && (inputWithdrawl > 0)){
+        bankCustomer.subtractBalance(inputWithdrawl);
+    }
+    else{
+      bankCustomer.addBalance(inputDeposit);
+      bankCustomer.subtractBalance(inputWithdrawl);
+    };
+
+    $(".userBalance").text(bankCustomer.showBalance());
+  });
 });
